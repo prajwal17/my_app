d284e13073d2147978430b3d7dafbb886f823e5d
Prajwal <babukumar9743@gmail.com>
2015-07-02 17:28:24 +0530

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+class CreateUsers < ActiveRecord::Migration
+def self.up
+create_table :users do |t|
+t.string :name
+t.string :email
+t.timestamps
+end
+end
+def self.down
+drop_table :users
+end
+end
