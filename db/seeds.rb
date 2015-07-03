709f8d322afc8eaa6086b1f3f5ec59442de5c42d
Prajwal <babukumar9743@gmail.com>
2015-07-03 16:24:29 +0530

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,3 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-class CreateUsers < ActiveRecord::Migration
-def self.up
-create_table :users do |t|
-t.string :name
-t.string :email
-t.timestamps
-end
-end
-def self.down
-drop_table :users
-end
-end
