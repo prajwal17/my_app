47ccf8e7b6c00dc8086f7a73453bca8fb045c4d5
Prajwal <babukumar9743@gmail.com>
2015-07-16 16:43:41 +0530

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,16 @@ class User < ActiveRecord::Base
 validates :email,  :presence => true,
                    :format => { :with => email_regex },
                    :uniqueness => { :case_sensitive => false }
+                   validates :password, :presence => true,
+                                        :confirmation => true,
+                                        :length => { :within => 6..40 }
+                                        before_save :encrypt_password
+private
+def encrypt_password
+self.encrypted_password = encrypt(password)
+end
+def encrypt(string)
+string # Only a temporary implementation!
+end
 
 end
